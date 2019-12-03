1a704690764b37f4f4fa86f7a091cf32fe8c37f5
Ibrahim Cesar Souza <ibrahim.souza@nexojornal.com.br>
2019-12-03 19:18:01 -0300

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { LiteYouTubeEmbed } from "./lib";
 
 const App = () => (
   <div style={{ width: 640, margin: "15px auto", border: "1px solid tomato" }}>
-   <iframe width="560" height="315" src="https://www.youtube.com/embed/L2vS_050c-M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <LiteYouTubeEmbed id="L2vS_050c-M" />
   </div>
 );
 
