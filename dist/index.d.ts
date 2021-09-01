3b2863327923339fa2799c72df1d8b2e225a29bf
Ibrahim Cesar <email@ibrahimcesar.com>
2021-09-01 09:42:19 -0300

    🐛 fix: type bug; property announce as optional

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,21 +1,27 @@
 /// <reference types="react" />
-declare type imgResolution = "default" | "mqdefault" | "hqdefault" | "sddefault" | "maxresdefault";
+declare type imgResolution =
+  | "default"
+  | "mqdefault"
+  | "hqdefault"
+  | "sddefault"
+  | "maxresdefault";
+
 interface LiteYouTube {
-    announce: string;
-    id: string;
-    title: string;
-    activatedClass?: string;
-    adNetwork?: boolean;
-    iframeClass?: string;
-    noCookie?: boolean;
-    cookie?: boolean;
-    params?: string;
-    playerClass?: string;
-    playlist?: boolean;
-    playlistCoverId?: string;
-    poster?: imgResolution;
-    wrapperClass?: string;
-    onIframeAdded?: () => void;
+  id: string;
+  title: string;
+  activatedClass?: string;
+  adNetwork?: boolean;
+  announce?: string;
+  iframeClass?: string;
+  noCookie?: boolean;
+  cookie?: boolean;
+  params?: string;
+  playerClass?: string;
+  playlist?: boolean;
+  playlistCoverId?: string;
+  poster?: imgResolution;
+  wrapperClass?: string;
+  onIframeAdded?: () => void;
 }
 export default function LiteYouTubeEmbed(props: LiteYouTube): JSX.Element;
 export {};
